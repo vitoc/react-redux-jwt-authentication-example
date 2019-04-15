bbfcbb85b7a712e549b215c0f8ad827ecc01650e
Vito Chin <vitochin@microsoft.com>
2019-04-15 17:53:03 +0800

    More auth options to consider

--- src/_helpers/auth-url.js
+++ src/_helpers/auth-url.js
@@ -1,3 +1,3 @@
 import { authConstants } from '../_constants';
 
-export const authUrl = `https://login.microsoftonline.com/${authConstants.TENANT_ID}/oauth2/authorize?client_id=${authConstants.APPLICATION_ID}&response_type=id_token&redirect_uri=http://localhost:8080/login&response_mode=form_post&scope=openid&state=gustav&nonce=tesla`;
+export const authUrl = `https://login.microsoftonline.com/${authConstants.TENANT_ID}/oauth2/authorize?client_id=${authConstants.APPLICATION_ID}&response_type=id_token&redirect_uri=${authConstants.REDIRECT_URI}/login&response_mode=form_post&scope=openid&state=${authConstants.STATE}&nonce=${authConstants.NONCE}`;
