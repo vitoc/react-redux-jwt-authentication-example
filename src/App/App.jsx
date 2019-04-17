27405e1736381ef982761d23a3b0d8952c14fa03
Vito Chin <vitochin@microsoft.com>
2019-04-17 15:48:31 +0800

    New reducer for user token

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,6 +8,7 @@ import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 
+
 class App extends React.Component {
     constructor(props) {
         super(props);
