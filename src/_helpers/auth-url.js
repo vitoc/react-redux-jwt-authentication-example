c61ef9ad14a47c2b4c2aaccd814345a36930c091
Vito Chin <vitochin@microsoft.com>
2019-04-17 17:38:48 +0800

    Use state to store uniqid used to retrieve token from backend

--- src/_helpers/auth-url.js
+++ src/_helpers/auth-url.js
@@ -1,3 +1,15 @@
 import { authConstants } from '../_constants';
+let uniqid = require('uniqid');
 
-export const authUrl = `https://login.microsoftonline.com/${authConstants.TENANT_ID}/oauth2/authorize?client_id=${authConstants.APPLICATION_ID}&response_type=id_token&redirect_uri=${authConstants.REDIRECT_URI}/login&response_mode=form_post&scope=openid&state=${authConstants.STATE}&nonce=${authConstants.NONCE}`;
+/**
+ * STATE: A value included in the request that is returned in the token response. 
+ * It can be a string of any content that you wish. A randomly generated 
+ * unique value is typically used for preventing cross-site request forgery 
+ * attacks. The state is also used to encode information about the user's 
+ * state in the app before the authentication request occurred, such as the 
+ * page or view they were on.
+ */
+
+let state = uniqid();
+localStorage.setItem('state', state);
+export const authUrl = `https://login.microsoftonline.com/${authConstants.TENANT_ID}/oauth2/authorize?client_id=${authConstants.APPLICATION_ID}&response_type=id_token&redirect_uri=${authConstants.REDIRECT_URI}&response_mode=form_post&scope=openid&state=${state}&nonce=${authConstants.NONCE}`;
